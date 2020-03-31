3a9a9bf2ce506fe062d6736958dff3cecf3ed9ef
João Victor <jvcostagoncalves12@gmail.com>
2020-03-31 15:20:47 +0000

    Finalizando aula 23

--- Capítulo_3-SKLearn-Pandas/processamento_censo.py
+++ Capítulo_3-SKLearn-Pandas/processamento_censo.py
@@ -15,7 +15,7 @@ base = pd.read_csv('census.csv')
 previsores = base.iloc[:, 0:14].values
 classe = base.iloc[:,14].values
 
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 #transformar as informações que estão em string para o formato discreto
 labelEncoder_previsores = LabelEncoder()
@@ -30,3 +30,11 @@ previsores[:, 7] = labelEncoder_previsores.fit_transform(previsores[:, 7])
 previsores[:, 8] = labelEncoder_previsores.fit_transform(previsores[:, 8])
 previsores[:, 9] = labelEncoder_previsores.fit_transform(previsores[:, 9])
 previsores[:, 13] = labelEncoder_previsores.fit_transform(previsores[:, 13])
+
+#inicialização de variável
+one_hot_encoder = OneHotEncoder(categories=[1,3,5,6,7,8,9,13])
+previsores = one_hot_encoder.fit_transform(previsores).toarray()
+
+#transformar a classe para o formato discreto
+label_classe = LabelEncoder()
+classe = label_classe.fit_transform(classe)
