6715e6adde6cd61cf1a0d0c07b6630c3b8bf3ae5
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-04-06 16:21:31 +0000

    Finalizando Aula 56

--- Capítulo_6-Aprendizagem_Por_Regra/credit_data_regras.py
+++ Capítulo_6-Aprendizagem_Por_Regra/credit_data_regras.py
@@ -6,4 +6,23 @@ Created on Mon Apr  6 15:53:53 2020
 @author: joao
 """
 
+import Orange
 
+base_de_dados = Orange.data.Table('original.csv')
+base_de_dados.domain
+
+base_dividida = Orange.evaluation.testing.sample(base_de_dados, n=0.25)
+base_treino = base_dividida[1]
+base_teste = base_dividida[0]
+
+len(base_treino)
+len(base_teste)
+
+cn2_learner = Orange.classification.rules.CN2Learner()
+classificador = cn2_learner(base_treino)
+
+for regras in classificador.rule_list:
+    print(regras)
+
+resultado = Orange.evaluation.testing.TestOnTestData(base_treino, base_teste, [classificador])
+print(Orange.evaluation.Precision(resultado))
